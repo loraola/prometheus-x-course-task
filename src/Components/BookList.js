93edecd86fc240d2bad34e74de1db416ea1ac8e0
loraolashyn <loraolashyn@gmail.com>
2023-08-17 22:26:15 +0300

    delete window.scrollTo(0,0)

--- src/Components/BookList.js
+++ src/Components/BookList.js
@@ -40,6 +40,7 @@ export default function BookList() {
 
     return (
         <main>
+             {window.scrollTo(0, 0)}
             <div className="container  d-flex searchFilter">
                 <div>
                     <input onInput={(e) => filterItems(e.target.value)} type="text" name="search" placeholder="Search by book name" id="search" />
